2ce44708182444ea726246962c0d7d4675507f59
Tamas Csaba <tamcsaba@gmail.com>
2016-03-21 16:06:04 +0100

    feat(mock): add readable extension like html and svg

--- mock.js
+++ mock.js
@@ -1,7 +1,14 @@
+var fs = require('fs');
 var extensions = ['.html', '.svg', '.sass', '.scss', '.css'];
+var readable = ['html', 'svg'];
+
+function isReadable(ext) {
+    return readable.indexOf(ext) >= 0;
+}
 
 for (var i = 0; i < extensions.length; i++) {
-  require.extensions[extensions[i]] = function(nodeModule) {
-    nodeModule.exports = '';
+  require.extensions[extensions[i]] = function(nodeModule, filename) {
+    var ext = filename.split('.').pop();
+    nodeModule.exports = isReadable(ext) ? fs.readFileSync(filename, 'utf8') : '';
   }
 }
